86c2cc354ec1cf63ab6be743aef5af3a22108226
az <azizkoura@hotmail.com>
2019-05-16 19:44:49 +0200

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <nuxt />
+  </div>
+</template>
+
+<style>
+html {
+  font-family: 'Dancing Script', cursive ,sans-serif;
+  
+}
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+
+</style>
